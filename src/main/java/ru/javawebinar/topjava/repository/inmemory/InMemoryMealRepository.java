cff6b47282e802b7be264f6d7aea2d6ed7059922
Vadim <h4998998482@gmail.com>
2022-02-17 17:10:29 +0300

    Create userId and modified method save

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,82 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+
 public class InMemoryMealRepository implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
+
+    static List<Meal> list = new ArrayList<>(Arrays.asList(
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+            new Meal(authUserId(), LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)));
+
 
     {
         MealsUtil.meals.forEach(this::save);
     }
 
+    public static void main(String[] args) {
+        InMemoryMealRepository inMemoryMealRepository = new InMemoryMealRepository();
+        inMemoryMealRepository
+                .save(new Meal(null,1, LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 20000));
+        System.out.println(list);
+    }
+
     @Override
     public Meal save(Meal meal) {
-        if (meal.isNew()) {
+        if (meal.isNew() && meal.getUserId() == authUserId()) {
             meal.setId(counter.incrementAndGet());
             repository.put(meal.getId(), meal);
             return meal;
+        } else {
+            return list.stream().filter(m -> meal.getUserId() == authUserId()).findFirst().orElse(null);
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+//            return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal)
     }
 
     @Override
     public boolean delete(int id) {
+        log.info("delete {}", id);
         return repository.remove(id) != null;
     }
 
     @Override
     public Meal get(int id) {
+        log.info("get {}", id);
         return repository.get(id);
     }
 
     @Override
     public Collection<Meal> getAll() {
+        log.info("getAll");
         return repository.values();
+    }
+
+    @Override
+    public String toString() {
+        return "InMemoryMealRepository{" +
+                "repository=" + repository +
+                ", counter=" + counter +
+                '}';
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,17 +6,23 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
@@ -28,18 +34,20 @@ public class InMemoryUserRepository implements UserRepository {
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return repository.values().stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return repository.values().stream().filter(user -> Objects.equals(user.getEmail(), email)).findAny().get();
     }
 }
